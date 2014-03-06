48154099560139f380ea3b1681136b2db50d7ce7
vikre <vikre.90@gmail.com>
2014-03-06 13:21:43 +0100

    Its a working chrome extension, for lazy students

--- background.js
+++ background.js
@@ -1,13 +1,3 @@
-/*chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' into something');
-  chrome.tabs.executeScript({
-    code: 'document.body.innerHTML = "<p style=\"text-align:center; font-size:88px;\">One way to itsLearning</p>";'
-  });
-});*/
 
-var hack = '<div class="wrapper"><a class="its" href="https://sats.itea.ntnu.no/sso-wrapper/web/wrapper?target=itslearning">One way to itsLearning</a></div>';
-
-/*var hack1 = '<a href="#">LOL</a>';*/
-/*Magic!*/
- $("body").html(hack);
+var action_url = "https://sats.itea.ntnu.no/sso-wrapper/web/wrapper?target=itslearning";
+chrome.extension.sendRequest({redirect: action_url}); // send message to redirect
